3cea65a7b2dad90d4d88b20cff87a59a68a05dd2
Fabian Rios <rocaboca@gmail.com>
2016-05-22 23:18:36 +0200

    Bumped version

--- toon.rb
+++ toon.rb
@@ -1,8 +1,8 @@
 class Toon < Formula
   desc "Interactive SoundCloud radio"
   homepage "https://github.com/yemi/toon"
-  url "https://github.com/yemi/toon/releases/download/v0.1.0/toon-0.1.0-osx"
-  version "0.1.0"
+  url "https://github.com/yemi/toon/releases/download/v0.1.1/toon-0.1.1-osx"
+  version "0.1.1"
   sha256 "cc9eaa16b202fae80b5fb2694657ecf42566538889ac8ca37aefbe8cb11876cc"
 
   def install
